a0b4acb94c21507d1520f8f5de03bdb674b91ccb
JoanaMota <youremail@domain.com>
2017-11-23 00:57:01 +0000

    Correct other stupid mistakes

--- hello.c
+++ hello.c
@@ -2,8 +2,8 @@
 int main()
 {
    // printf() displays the string inside quotation
-    printf("Hello World!");
-    printf("adaoooo!!")
-    printf( "lolllllll  --- Just another change");
+    printf("Hello World!\n");
+    printf("adaoooo!!\n");
+    printf( "lolllllll  --- Just another change\n");
     return 0;
 }
